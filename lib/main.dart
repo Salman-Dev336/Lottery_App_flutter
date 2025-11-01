c46e4fe8d0767e211a3ba062939bef01fee14349
Salman-Dev336 <salmantechhub336@gmail.com>
2025-11-02 03:34:17 +0500

    last changes

--- lib/main.dart
+++ lib/main.dart
@@ -36,7 +36,7 @@ class _MyAppState extends State<MyApp> {
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
             Center(
-              child: Text('Your winning Number is : $x',style: TextStyle(
+              child: Text('Your winning Number is :7',style: TextStyle(
                 fontSize: 20,
                 fontWeight: FontWeight.bold,
               ),),
@@ -44,19 +44,33 @@ class _MyAppState extends State<MyApp> {
             SizedBox(height: 15,),
             Container(
               height: 250,
+              width: 300,
               decoration: BoxDecoration(
-                color: Colors.lightGreenAccent,
+                color: x==7?Colors.lightGreenAccent:Colors.yellowAccent,
                 borderRadius: BorderRadius.circular(15),
               ),
               child: Padding(
                 padding: const EdgeInsets.all(20),
-                child: Column(
+                child: x == 7?Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Icon(Icons.done_all,color: Colors.blue,size: 30,),
+                    SizedBox(height: 10,),
+                    Text('Congratulation You have WIN The Lottery\n Your Number is $x \n ',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                      ),),
+                  ],
+                )
+                    :Column(
                   mainAxisAlignment: MainAxisAlignment.center,
                   crossAxisAlignment: CrossAxisAlignment.center,
                   children: [
                     Icon(Icons.error,color: Colors.red,size: 30,),
                     SizedBox(height: 10,),
-                    Text('Better Luck Next Time Your Winning Number is $x\n try again',
+                    Text('Better Luck Next Time Your Number is $x \n try again',
                     textAlign: TextAlign.center,
                     style: TextStyle(
                       fontWeight: FontWeight.bold,
@@ -68,7 +82,11 @@ class _MyAppState extends State<MyApp> {
           ],
         ),
         floatingActionButton: FloatingActionButton(onPressed: (){
-          print(random.nextInt(10).toString());
+         x = random.nextInt(30);
+         print(x);
+         setState(() {
+
+         });
         },
           child: Icon(Icons.refresh_rounded),
           backgroundColor: Colors.pinkAccent,
