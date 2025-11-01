17d626516f3a89527b420439a0249af33411e905
Salman-Dev336 <salmantechhub336@gmail.com>
2025-11-02 01:23:34 +0500

    added app bar and button

--- lib/main.dart
+++ lib/main.dart
@@ -10,9 +10,25 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
+        appBar: AppBar(
+          title: Text('My first App'),
+          backgroundColor: Colors.blue,
+        ),
         body: SafeArea(
-            child: Text('salman')),
+            child: Column(
+              children: [
+              ],
+            )
+        ),
+        floatingActionButton: FloatingActionButton(onPressed:
+            () {
+          print('salman');
+        },
+          child: Icon(Icons.add),
+          backgroundColor: Colors.blue,
+        ),
       ),
     );
   }
